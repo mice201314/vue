9860cdbfc21c20576dd78aab97cb5d50cda1100c
mice201314 <37531932+mice201314@users.noreply.github.com>
2019-04-28 14:47:15 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -13,26 +13,29 @@
         return userAgent.indexOf("MAC OS") != -1;
     })();
 
-    var nativeBridge = (function() {
-        return function(name, param, cbName) {
-            if (isAnroid) {
-                window.JSInterface[name](cbName, JSON.stringify(param));
-            } else if (isIos) {
-                window[name](cbName, JSON.stringify(param));
-            }
-        };
-    })();
+    var nativeBridge = function(name, param, cbName) {
+        if (isAnroid) {
+            window.JSInterface[name](cbName, JSON.stringify(param));
+        } else if (isIos) {
+            window[name](cbName, JSON.stringify(param));
+        }
+    };
 
     var callNative = function(name, param, callback) {
         if (typeof callback != "function") {
             callback = param;
         }
-        var cbName = "_NATIVE_CB_" + native[key];
+        var cbName = "_NATIVE_CB_" + name;
         window[cbName] = function(data) {
             var jsonData = typeof data == "string" ? JSON.parse(data) : data;
             callback(jsonData);
         };
-        return nativeBridge(name, param, cbName);
+        try {
+            nativeBridge(name, param, cbName);
+        } catch {
+            console.warn("非App环境!");
+            window[cbName]();
+        }
     };
 
     var native = {
@@ -41,7 +44,7 @@
     };
     for (var key in apiNames) {
         native[key] = function(param, callback) {
-            return callNative(apiNames[key], param, callback);
+            callNative(apiNames[key], param, callback);
         };
     }
 
