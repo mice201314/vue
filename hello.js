6fc8d9f49039f7677099a5e48f6cb6b8216b0f13
mice201314 <37531932+mice201314@users.noreply.github.com>
2019-04-28 12:22:53 +0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,163 +1,49 @@
-//建立native的方法名和id的映射, 方便查看
-const BACK_TO_APP = 'backToApp';
+(function() {
+    var apiNames = {
+        share: "AppShare",
+        scan: "AppScan",
+        backToApp: "backToApp"
+    };
 
-// 有回调的方法
-const HAVE_CALLBACK_NAMES = [
-    BACK_TO_APP,
-    SET_NAVIGATION_BAR_BASE,
-    SET_TALKING_DATA,
-    GET_STEP_COUNTBYTIME,
-    GET_SYSTEM_PATH,
-    GET_USER_INFO,
-    GO_TO_LOGIN_PAGE,
-    GO_TO_QY_SESSION,
-    GO_WX_MIN_PROGRAM,
-    GET_SPORT_BURN_CALORIES
-];
-// 没有回调的方法
-const NO_CALLBACK_NAMES = [
-    GO_TO_HOSPITAL_LISTS,
-    GO_TO_MEMBER_CARD,
-    GO_TO_BING_INSURANCE,
-    IS_SHOW_PROGRESS_DIALOG,
-    GO_TO_PAGE
-];
+    var userAgent = window.navigator.userAgent.toUpperCase();
+    var isAnroid = (function() {
+        return userAgent.indexOf("ANDROID") != -1;
+    })();
+    var isIos = (function() {
+        return userAgent.indexOf("MAC OS") != -1;
+    })();
 
-const NAMES = HAVE_CALLBACK_NAMES.concat(NO_CALLBACK_NAMES);
-
-
-
-
-
-const IS_DEBUG = process.env.NODE_ENV.trim() === 'development';
-
-//取userAgent
-const ua = window.navigator.userAgent.toUpperCase();
-
-const isAndroidFunc = function (ua) {
-    return ua.indexOf('ANDROID') !== -1;
-};
-
-const isIosFunc = function (ua) {
-    return ua.indexOf('MAC OS') !== -1;
-};
-const isIos11Fun = function (ua) {
-    return ua.indexOf("OS 11") !== -1;
-
-};
-const isAndroid = isAndroidFunc(ua);
-
-const isIos = isIosFunc(ua);
-const isIOS11 = function(){return isIos11Fun(ua)};
-const createRandomNum = function (num) {
-    const arr = [ ];
-    const $num = num || 6;
-
-    for(let i = 0; i < $num; i++) {
-        arr.push(~~(Math.random()*10));
-    }
-
-    return arr.join("");
-};
-
-var createCbname = function (name, num) {
-    return "_NATIVE_CB_" + name + "_" + createRandomNum(num);
-};
-
-var iosCallNative = function (name, cbname, opt) {
-    if(!opt){
-      var opt = {};
-    }
-    opt.callBackName = cbname;
-    window.webkit.messageHandlers[name].postMessage(opt);
-};
-
-var androidCallNative = function (name, cbname, opt) {
-
-    if( opt ) {
-        window.AndroidJSInterface[ name ]( JSON.stringify(opt), cbname );
-    } else {
-        window.AndroidJSInterface[ name ]( cbname );
-    }
-};
-
-let callNative;
-
-if (isAndroid) {
-    callNative = androidCallNative;
-} else if (isIos) {
-    callNative = iosCallNative;
-}
-
-var callNative$1 = function (name, opt) {
-    return  new Promise(function (resolve, reject) {
-        const cbname = createCbname(name);
-
-        window[cbname] = async function (data) {
-          await resolve(data);
-          //这里进行销毁
-          await (window[cbname] = null);
-          // todo reject
+    var nativeBridge = (function() {
+        return function(name, param, cbName) {
+            if (isAnroid) {
+                window.JSInterface[name](cbName, JSON.stringify(param));
+            } else if (isIos) {
+                window[name](cbName, JSON.stringify(param));
+            }
         };
+    })();
 
-        try{
-          callNative(name, cbname, opt);
-        }catch (e){
-          if(name == "getSystemPath"){
-            let version = location.href.match(/appVersion=([\w\.]+)/);
-            window[cbname]({statusCode:0,data:{deviceID:"no deviceID","appVersion":version?version[1]:new Date().getTime()}});
-          }else if(name == "goWXMinProgram"){
-              console.log("no goWXMinProgram");
-          }else{
-            window[cbname]();
-          }
-
+    var callNative = function(name, param, callback) {
+        if (typeof callback != "function") {
+            callback = param;
         }
-    });
-};
+        var cbName = "_NATIVE_CB_" + native[key];
+        window[cbName] = function(data) {
+            var jsonData = typeof data == "string" ? JSON.parse(data) : data;
+            callback(jsonData);
+        };
+        return nativeBridge(name, param, cbName);
+    };
 
-const native = { };
-//是否为IOS11
-native.isIOS11 = isIOS11;
-// 给native扩展方法
-const extendNativeMethod = function () {
-    for (let i = 0, len = NAMES.length; i < len; i++) {
-        let name = NAMES[i];
-
-        native[name] = function (pramas) {
-            
-            return callNative$1(name, pramas);
+    var native = {
+        isAnroid: isAnroid,
+        isIos: isIos
+    };
+    for (var key in apiNames) {
+        native[key] = function(param, callback) {
+            return callNative(apiNames[key], param, callback);
         };
     }
-};
 
-extendNativeMethod();
-
-let language = '';
-let languageSuffix = '';
-// 设置语言
-native.setLanguage = function (lang) {
-    language = lang.toLocaleLowerCase();
-    languageSuffix = '_' + language;
-};
-// 附加更语义化的API, 调用extend方法附加的API,
-// 尽量避免参数有魔鬼数字和Boolean值
-/* 导航栏相关 */
-// 设置导航栏标题
-native.setNavigationTitle = function (title, fn) {
-    var cb = fn || function(){};
-    return this[SET_NAVIGATION_BAR_BASE]({
-        actionType: '3',
-        value: title
-    }, cb);
-};
-
-/* 判断机器类型 */
-native.isIos = function () {
-    return isIos;
-};
-native.isAndroid = function () {
-    return isAndroid;
-};
-
-export default native;
+    window.Native = native;
+})();
